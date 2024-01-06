879540fa86e82b5a527d120e92875278280eeb9e
NewSensation <144243147+NewSensation@users.noreply.github.com>
2024-01-06 17:47:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function generateConfetti() {
     for (let i = 0; i < 50; i++) {
         const confetti = document.createElement('div');
         confetti.className = 'confetti';
-        confetti.style.left = `${Math.random() * 100}vw`;
+        confetti.style.left = `${Math.random() * 45}vw`;
         confetti.style.animationDuration = `${Math.random() * 2 + 1}s`;
         confettiContainer.appendChild(confetti);
     }
